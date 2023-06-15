1deaf0f954fa4a9fa87ee1fccbb2645e0f3089f5
sebvc <90735870+sebvc@users.noreply.github.com>
2023-06-14 19:15:37 -0500

    Add REPL Message Refference file

---
+++ Unicodehelptext.jl
@@ -0,0 +1,18 @@
+# This file is written in of Julia.
+
+function main()
+    println("help?>\n", # you can type '?' for this text to show up
+    "search:  ] [ = \$ ; ( { ) ? . } ⊻ ⊋ ⊊ ⊉ ⊈ ⊇ ⊆ ≥ ≤ ≢ ≡ ≠ ≉ ≈ ∪ ∩ ∛ √ ∘ ∌ ∋ ∉ ∈ ℯ π ÷ ~ | ^ \\ > < : / - + * ' & % ! && if
+
+    Welcome to Julia 1.6.3. The full manual is available at
+
+    https://docs.julialang.org
+
+    as well as many great tutorials and learning resources:
+
+    https://julialang.org/learning/
+
+    For help on a specific function or macro, type ? followed by its name, e.g. ?cos, or ?@time, and press enter. Type ; to enter shell mode, ] to enter package mode.")    
+end
+
+main()
